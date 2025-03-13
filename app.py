4d522fc34a963244a3a1a65630c8a6a7b346b990
VVignesh <vvkumarpy@gmail.com>
2025-03-13 17:10:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello, World ! - Pyflask Demo</h1>'
+
+@app.route('/version')
+def get_version():
+    return '<h1>App version : <b>2.9</b></h1>'
+
+@app.route('/test')
+def get_test():
+    return '<h1>Welcome! You are accessing /new_test1 endpoint</h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
